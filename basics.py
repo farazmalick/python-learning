8efcf4a48573e780dd0b20e4d40c503106674342
Faraz Aslam <farazmalik52@gmail.com>
2020-08-06 12:28:41 +0500

    basic-data-types

---
+++ basics.py
@@ -0,0 +1,68 @@
+#import datetime
+#var=datetime.datetime.now()
+#print(var)
+
+#a=10
+#b="ali"
+#c=10.1
+#print(type(a),type(b),type(c))
+
+#list
+
+"""grades=[12,20,50]
+print(grades)
+print(type(grades))
+grades.append(30)
+print(grades)
+grades.insert(0,22)
+print(grades)
+grades.remove(50)
+print(grades)
+grades.pop()
+print(grades)
+
+list1 = [1.2323442655, 1.4534345567, 1.023458894]
+
+list2 = [1.9934332091]
+list2.append(list1[2])
+print(list1,list2)
+"""
+#tuple
+"""
+g= 1,2,3
+grades=([12,20,50])
+#grades=([12,20,50],[12,20,50])
+#s=sum(grades)/len(grades)
+#print(s)
+print(g)
+print(type(g))
+
+mylist = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 
+		'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+Firstitem=mylist[0]
+Lastitem=mylist[-1]
+c=Firstitem+Lastitem
+print(c)
+"""
+#set
+"""
+d={1,2,3}
+a = set('ab')
+b=set('bc')
+print(a,b,d)
+print(a-b)
+print(a|b)
+print(a&b)
+print(a^b)
+"""
+#dictionary
+"""
+grades={"ali":20,"ahmad":10}
+print(type(grades))
+print(grades.values())
+print(grades.keys())
+print(list(grades))
+#grades.update(["ali":22])
+
+"""
