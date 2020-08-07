4f46ee3b52255cd53eda735990fbe2f927082b66
Faraz Aslam <farazmalik52@gmail.com>
2020-08-07 20:19:38 +0500

    py-file-handling

---
+++ filehandling.py
@@ -0,0 +1,34 @@
+#fileObj = open("files/fruits.txt")
+# print(fileObj.read())
+# print(fileObj.read())
+
+#content = fileObj.read()
+# fileObj.close()
+# print(content)
+"""
+with open("files/fruits.txt") as fileObj:
+    content = fileObj.read()
+print(content)
+
+with open("files/vegetables", "a") as fileObj:
+    fileObj.write("\npotato")
+"""
+"""
+with open("files/vegetables", "a+") as fileObj:
+    fileObj.write("\npotato")
+    fileObj.seek(0)
+    content = fileObj.read()
+
+print(content)
+"""
+
+import time
+import os
+import pandas
+while True:
+    if os.path.exists("files/temps_today.csv"):
+        data = pandas.read_csv("files/temps_today.csv")
+        print(data.mean()["st1"])
+    else:
+        print("err:file not found")
+    time.sleep(3)
