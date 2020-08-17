c9430124d391a103b39ed3cd357d27762b0cc173
Faraz Aslam <farazmalik52@gmail.com>
2020-08-17 20:07:44 +0500

    webcam-motion-detection

---
+++ computervision/videocapture.py
@@ -0,0 +1,12 @@
+import cv2
+
+video = cv2.VideoCapture(1)
+while True:
+    check, frame = video.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    cv2.imshow("capture", gray)
+    key = cv2.waitKey(1)
+    if key == ord('q'):
+        break
+video.release()
+cv2.destroyAllWindows()
